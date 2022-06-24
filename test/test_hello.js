ee72e62c45b421193ebd36a2349b7b1e223942c6
exopyl <exopyl74@mail.ru>
2022-06-24 23:27:04 +0200

    add test

--- test/test_hello.js
+++ test/test_hello.js
@@ -7,7 +7,17 @@ const assert = require('assert');
 describe('Hello Test', () => {
     it('should return Hello world', () => {
            strHello = addon.hello()
-           assert.equal(strHello, "Hello World");
+           assert.equal(strHello, "Hello World")
+       });
+   });
+
+
+
+   describe('Hello Test', () => {
+    it('should return Hello world', () => {
+            const dog = new addon.Dog()
+            str = dog.Speak()
+            assert.equal(str, "wouaf");
        });
    });
 
